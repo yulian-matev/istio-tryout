6523198f4db851506e3285566177f52c98fcb949
Yulian Matev <yulian_matev@abv.bg>
2024-01-25 17:51:00 +0200

    CI: Verify code against .editorconfig

--- src/yellow-svc/php/api.php
+++ src/yellow-svc/php/api.php
@@ -1,20 +1,29 @@
 <?php
 
 define("SERVICE_NAME",  "yellow");
-define("SERVICE_VER",   "1.0");
+define("SERVICE_VER",   "1.1");
 
 
+$seq_counter = 1;
+if( apcu_exists('seq_counter' ) ) {
+    $seq_counter = apcu_fetch('seq_counter');
+} 
 
 
 $arr = array(
                 'service-name'  => SERVICE_NAME,
                 'version'       => SERVICE_VER,
                 'commit-sha'    => 'xxxxxxx',
-                'now'           => date('Y-m-d h:i:s') 
+                'now'           => date('Y-m-d h:i:s'),
+                'seq-counter:'  => $seq_counter
             );
 
 
-echo json_encode($arr);
+$seq_counter++;
+apcu_store('seq_counter', $seq_counter);
 
+
+
+echo json_encode($arr);
 
 ?>
