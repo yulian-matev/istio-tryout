6b17358d324a298023b1317a58eb943c2644fdc1
Yulian Matev <yulian_matev@abv.bg>
2024-01-26 16:08:53 +0200

    Use only one service - micro-svc

--- src/micro-svc/php/api.php
+++ src/micro-svc/php/api.php
@@ -1,17 +1,22 @@
 <?php
 
-define("SERVICE_NAME",  "yellow");
 define("SERVICE_VER",   "1.1");
 
-
+# Load sequence counter
 $seq_counter = 1;
 if( apcu_exists('seq_counter' ) ) {
     $seq_counter = apcu_fetch('seq_counter');
 }
 
+# Try to detect service name from environment or default to 'black'
+$service_name = 'black';
+if( getenv("SERVICE_NAME") !== false ) {
+    $service_name = $_ENV['SERVICE_NAME'];
+}
+
 
 $arr = array(
-                'service-name'  => SERVICE_NAME,
+                'service-name'  => $service_name,
                 'version'       => SERVICE_VER,
                 'commit-sha'    => 'xxxxxxx',
                 'now'           => date('Y-m-d h:i:s'),
@@ -19,12 +24,13 @@ $arr = array(
             );
 
 
+# Increment and store sequence counter
 $seq_counter++;
 apcu_store('seq_counter', $seq_counter);
 
 
 
-echo json_encode($arr);
+echo json_encode($arr, JSON_PRETTY_PRINT);
 
 ?>
 
